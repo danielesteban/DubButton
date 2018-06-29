aafb1b9099b376ed4f15c467edb80274834fe01b
Daniel Esteban Nombela <dani@gatunes.com>
2018-06-29 20:51:49 +0200

    Moar keywords

--- DubButton/keywords.c
+++ DubButton/keywords.c
@@ -10,6 +10,23 @@ const char keyword_5[] PROGMEM = "roots";
 const char keyword_6[] PROGMEM = "nintendo";
 const char keyword_7[] PROGMEM = "chill";
 const char keyword_8[] PROGMEM = "downtempo";
+const char keyword_9[] PROGMEM = "vapor";
+const char keyword_10[] PROGMEM = "dreamy";
+const char keyword_11[] PROGMEM = "sweet";
+const char keyword_12[] PROGMEM = "relax";
+const char keyword_13[] PROGMEM = "meditation";
+const char keyword_14[] PROGMEM = "deep";
+const char keyword_15[] PROGMEM = "ambient";
+const char keyword_16[] PROGMEM = "adhd";
+const char keyword_17[] PROGMEM = "beats";
+const char keyword_18[] PROGMEM = "nu";
+const char keyword_19[] PROGMEM = "jazz";
+const char keyword_20[] PROGMEM = "drum";
+const char keyword_21[] PROGMEM = "bass";
+const char keyword_22[] PROGMEM = "break";
+const char keyword_23[] PROGMEM = "core";
+const char keyword_24[] PROGMEM = "modern";
+const char keyword_25[] PROGMEM = "old";
 
 const char* const keywords[] PROGMEM = {
   keyword_0,
@@ -21,6 +38,23 @@ const char* const keywords[] PROGMEM = {
   keyword_6,
   keyword_7,
   keyword_8,
+  keyword_9,
+  keyword_10,
+  keyword_11,
+  keyword_12,
+  keyword_13,
+  keyword_14,
+  keyword_15,
+  keyword_16,
+  keyword_17,
+  keyword_18,
+  keyword_19,
+  keyword_20,
+  keyword_21,
+  keyword_22,
+  keyword_23,
+  keyword_24,
+  keyword_25,
 };
 
 const uint8_t num_keywords = sizeof(keywords) / sizeof(*(keywords));
