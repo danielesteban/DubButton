af7e9c69c4b2e844503680c1101087646b591b65
Daniel Esteban Nombela <dani@gatunes.com>
2018-06-29 05:57:07 +0200

    Add monitor sketch to the repository

---
+++ monitor/monitor.ino
@@ -0,0 +1,60 @@
+// This is a simple sketch to debug/monitor the IR output of the DubButton.
+// Upload it to an arduino board that has a hardware serial,
+// Connect an IR Receiver to it and open the serial monitor.
+
+#include <IRremote.h>
+
+#define RECEIVER_PIN 11
+IRrecv irrecv(RECEIVER_PIN);
+
+// Samsung TV IR codes
+#define POWER 0xE0E040BF
+#define MUTE 0xE0E0F00F
+#define SMART_HUB 0xE0E09E61
+#define UP 0xE0E006F9
+#define LEFT 0xE0E0A659
+#define RIGHT 0xE0E046B9
+#define DOWN 0xE0E08679
+#define ENTER 0xE0E016E9
+
+void setup() {
+  // Start receiving codes
+  Serial.begin(115200);
+  irrecv.enableIRIn();
+}
+
+void loop() {
+  decode_results results;
+  if (irrecv.decode(&results)) {
+    if (results.decode_type == SAMSUNG) {
+      // Dump the code
+      switch (results.value) {
+        case POWER:
+          Serial.println("POWER");
+          break;
+        case MUTE:
+          Serial.println("MUTE");
+          break;
+        case SMART_HUB:
+          Serial.println("SMART_HUB");
+          break;
+        case LEFT:
+          Serial.println("LEFT");
+          break;
+        case RIGHT:
+          Serial.println("RIGHT");
+          break;
+        case DOWN:
+          Serial.println("DOWN");
+          break;
+        case ENTER:
+          Serial.println("ENTER");
+          break;
+        default:
+          Serial.println("UNKNOWN");
+          break;
+      }
+    }
+    irrecv.resume();
+  }
+}
